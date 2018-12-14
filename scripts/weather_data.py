2eb46c72433c8b7ed2d81502918f137fed99184a
Yacin Tmimi <yacintmimi@gmail.com>
2018-12-13 20:14:01 -0500

    filled out helper functions for making api requests, parsing csv, and generating new csv

--- scripts/weather_data.py
+++ scripts/weather_data.py
@@ -2,7 +2,9 @@ import os
 import json
 import csv
 import requests
+from functools import wraps
 
+# hard code the API_KEY if not loading it from a .env file using pipenv
 API_KEY = os.environ['OPEN_WEATHER_API_KEY']
 
 #see docs: https://openweathermap.org/current#name
@@ -17,8 +19,10 @@ def _city_weather(city, country_code='us'):
 	return: (dict) dictionary containing weather info
 	'''
 	# url parameters
+	# addtional API url parameters https://openweathermap.org/current#other
 	params = {
-		'q':','.join([city, country_code]),
+		'q':','.join([city, country_code]), # for accurate city search
+		'units':imperial, # for Fahrenheit temp units
 		'APPID': API_KEY,
 	}
 	# makes the API requests and checks that the response is OK
@@ -29,38 +33,57 @@ def _city_weather(city, country_code='us'):
 
 def _weather_dict_from_csv(csv_path):
 	'''
-	csv_path: (str) path to the csv with location data
+	csv_path: (str) full path and file name to the csv with location data
 	return: (dict) a dictionary of {city_name: weather_data, ...}
 	'''
-	#check that the csv_file exists
-
+	#verify csv_path is a csv file.
+	if csv_path[-4:] != '.csv':
+		raise ValueError(f'{csv_path} is not a CSV file.')
 	#Keep track of cities
 	cities = {}
+	try:
+		with open(csv_path, 'r') as file:
+			csv_file = csv.reader(file)
+	        # skip the header row
+	        next(csv_file)
+	        for row in csv_file:
+				print(row)
+				#get weather data from the API
+				data = _city_weather(row)
+				if data and row not in cities:
+					cities[row] = data
+	except FileNotFoundError:
+		print(f'{csv_path} not found')
+	return cities
 
-	#open the csv file
-	with open(csv_path, 'w') as file:
-		pass
 
-
-
-def generate_weather_csv(weather_dict, file_path):
+def _generate_weather_csv(weather_dict, file_path):
 	'''
-	weather_dict: (dict) a dictionary of {city_name: weather_data, ...}
+	weather_dict: (dict) in the form {city1: weather_data, city2: weather_data, ...}
 	file_path: full path to the newly generated csv
 	generates a new csv
 	'''
-	pass
+	header = ['Location', 'Temperature', 'Wind Speed', 'Weather Description']
+	with open(file_path, 'w') as file:
+		csv_file = csv.writer(file):
+		csv_file.writerow(header)
+		for city, data in weather_dict.items():
+			temp = data['main']['temp']
+			wind = data['wind']['speed']
+			desc = data['weather'][0]['description']
+			csv_file.writerow([city, temp, wind, desc])
 
 
-
-
-
-
-
-
-
-
+def weather_data_csv(csv_path):
+	'''
+	Given the path to a csv containing locatin data,output a new file (weather_data.csv)
+	in the same directory as the original csv The new csv file contains information
+	on Location, Temperature, Wind Speed, and a weather description
+	'''
+	new_file_path = os.path.join(os.path.dirname(csv_path), 'weather_data.csv')
+	weather_dict = _weather_dict_from_csv(csv_path)
+	_generate_weather_csv(weather_dict, new_file_path)
 
 
 if __name__ == '__main__':
-	print(weather_data_by_city('New York'))
+	pass
